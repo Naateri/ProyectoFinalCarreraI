8050157c9e6fe6d2d4dbcaf63f2eb17d46feeaec
Renato Postigo <renato.postigo@ucsp.edu.pe>
2020-06-17 23:51:17 -0500

    MR Dataset

--- KimCNN/main.py
+++ KimCNN/main.py
@@ -1,24 +1,37 @@
+## TODO:
+## -word2vec
+## -KFold for MR dataset
+## -SST2 dataset
+
 from KimCNN import KimCNN
 import numpy as np
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
+from sklearn.model_selection import train_test_split
 import os
-
-def transform(sentence, pad_len, words, UNK=2): # Pass sentence to integers
+# Pass sentence to integers
+def transform(sentence, pad_len, words, /, ret_array=True, UNK=2,
+		max_features=20000):
 	vector = list()
 	sentence = sentence.split()
 	for word in sentence:
 		if word.lower() not in words:
 			vector.append(UNK)
 		else:
-			vector.append(words[word.lower()])
+			if words[word.lower()] >= max_features:
+				vector.append(UNK)
+			else:
+				vector.append(words[word.lower()])
 	# Saving as numpy array
 	vector = np.array(vector) 
 	# Saving as a matrix
 	vector = np.array([vector])
 	# Padding
 	padded_vector = sequence.pad_sequences(vector, maxlen=pad_len)
-	return padded_vector
+	if ret_array:
+		return padded_vector
+	else:
+		return padded_vector[0]
 
 def get_wordvec_imdb():
 	vocab_file = 'datasets/aclimdb/imdb.vocab'
@@ -36,7 +49,41 @@ def get_wordvec_imdb():
 
 	return words, index
 
-def load_imdb():
+def load_MR(words, index):
+	pos_files = 'datasets/review_polarity/txt_sentoken/pos'
+	neg_files = 'datasets/review_polarity/txt_sentoken/neg'
+	
+	train_x = list()
+	train_y = list()
+	#words, index = get_wordvec_imdb() # For now
+
+	# train positives
+	print("MR positives")
+	for filename in os.listdir(pos_files):
+		#print(os.path.join(train_pos_files, filename))
+		# open in read_only mode
+		f = open(os.path.join(pos_files, filename), 'r')
+		#print(f.read())
+		array = transform(f.read(), 400, words, UNK=index, ret_array=False)
+		train_x.append(array)
+		train_y.append(1.0)
+		f.close()
+
+	print("MR negatives")
+	for filename in os.listdir(neg_files):
+		#print(os.path.join(train_pos_files, filename))
+		# open in read_only mode
+		f = open(os.path.join(neg_files, filename), 'r')
+		#print(f.read())
+		array = transform(f.read(), 400, words, UNK=index, ret_array=False)
+		train_x.append(array)
+		train_y.append(1.0)
+		f.close()
+
+	return train_x, train_y
+
+
+def load_imdb(words, index):
 	train_pos_files = 'datasets/aclimdb/train/pos'
 	train_neg_files = 'datasets/aclimdb/train/neg'
 	test_pos_files = 'datasets/aclimdb/test/pos'
@@ -47,7 +94,7 @@ def load_imdb():
 	test_x = list()
 	test_y = list()
 
-	words, index = get_wordvec_imdb()
+	#words, index = get_wordvec_imdb()
 	
 	# train positives
 	print("IMDB train positives")
@@ -56,7 +103,7 @@ def load_imdb():
 		# open in read_only mode
 		f = open(os.path.join(train_pos_files, filename), 'r')
 		#print(f.read())
-		array = transform(f.read(), 400, words, index)
+		array = transform(f.read(), 400, words, UNK=index, ret_array=False)
 		train_x.append(array)
 		train_y.append(1.0)
 		f.close()
@@ -68,7 +115,7 @@ def load_imdb():
 		# open in read_only mode
 		f = open(os.path.join(train_neg_files, filename), 'r')
 		#print(f.read())
-		array = transform(f.read(), 400, words, index)
+		array = transform(f.read(), 400, words, UNK=index, ret_array=False)
 		train_x.append(array)
 		train_y.append(0.0)
 		f.close()
@@ -80,7 +127,7 @@ def load_imdb():
 		# open in read_only mode
 		f = open(os.path.join(test_pos_files, filename), 'r')
 		#print(f.read())
-		array = transform(f.read(), 400, words, index)
+		array = transform(f.read(), 400, words, UNK=index, ret_array=False)
 		test_x.append(array)
 		test_y.append(1.0)
 		f.close()
@@ -92,7 +139,7 @@ def load_imdb():
 		# open in read_only mode
 		f = open(os.path.join(test_neg_files, filename), 'r')
 		#print(f.read())
-		array = transform(f.read(), 400, words, index)
+		array = transform(f.read(), 400, words, UNK=index, ret_array=False)
 		test_x.append(array)
 		test_y.append(0.0)
 		f.close()
@@ -106,14 +153,14 @@ maxlen = 400
 max_features = 20000
 network = KimCNN(400, max_features)
 
-#words = imdb.get_word_index()
-#words = {k:(v+3) for k,v in words.items()}
-#words["<PAD>"] = 0
-#words["<START>"] = 1
-#words["<UNK>"] = 2
-#words["<UNUSED>"] = 3
+imdb_words = imdb.get_word_index()
+imdb_words = {k:(v+3) for k,v in imdb_words.items()}
+imdb_words["<PAD>"] = 0
+imdb_words["<START>"] = 1
+imdb_words["<UNK>"] = 2
+imdb_words["<UNUSED>"] = 3
 
-words, _ = get_wordvec_imdb()
+words, imdb_index = get_wordvec_imdb()
 
 id_to_word = {value:key for key,value in words.items()}
 
@@ -125,34 +172,70 @@ get_sentence = lambda vector : ' '.join(id_to_word[num] for num in vector)
 #load_imdb()
 #exit()
 
+DATASET = 1
+
+# 0 -> keras imdb
+# 1 -> IMDB
+# 2 -> MR 
+
 if train:
 	print('Training model')
 	print('Loading data...')
-	#data = imdb.load_data(num_words=max_features)
-	data = load_imdb()
-	(x_train, y_train), (x_test, y_test) = data
+	if DATASET == 0:
+		data = imdb.load_data(num_words=max_features)
+		(x_train, y_train), (x_test, y_test) = data
+	elif DATASET == 1:
+		#data = load_imdb(words, imdb_index)
+		data = load_imdb(words, 2)
+		(x_train, y_train), (x_test, y_test) = data
+	elif DATASET == 2:
+		print("len(i_words)", len(imdb_words))
+		data = load_MR(imdb_words, 2)
+		X, y = data
+		x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-	# x_train, x_test = list of indexes
+	# x_train, x_test = list of indexes (words)
 	# y_train, y_test = list of 0 (neg) or 1 (pos)
 
 	print(len(x_train), 'train sequences')
 	print(len(x_test), 'test sequences')
 
 	print('Pad sequences (samples x time)')
-	x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-	x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+	if DATASET == 0:
+		x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+		x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+	
+	x_train = np.array(x_train)
+	x_test = np.array(x_test)
 	print('x_train shape:', x_train.shape)
 	print('x_test shape:', x_test.shape)
 	y_train = np.array(y_train)
 	y_test = np.array(y_test)
 
 	print("x shape: ", x_train[0].shape)
+	print("x_train[0]", x_train[0])
+	print("x_train[1]", x_train[1])
 	example = transform("That guy sucks really bad buddy", maxlen, words)
 	print("example shape", example.shape)
 	#print("example in array shape", np.array([example]).shape)
 
-	network.train(x_train, x_test, y_train, y_test, batch_size=50, epochs=1,
+	batch_size = 50
+	if DATASET == 0 or DATASET == 1:
+		epochs = 45
+	elif DATASET == 2:
+		epochs = 60
+
+	print("Ready to train")
+	#exit()
+	if DATASET == 0:
+		network.train(x_train, x_test, y_train, y_test, batch_size=batch_size, epochs=epochs,
+			save_model=True, save_file='models/kimcnn_model')
+	elif DATASET == 1:
+		network.train(x_train, x_test, y_train, y_test, batch_size=batch_size, epochs=epochs,
 			save_model=True, save_file='models/kimcnn_model_imdb')
+	elif DATASET == 2:
+		network.train(x_train, x_test, y_train, y_test, batch_size=batch_size, epochs=epochs,
+			save_model=True, save_file='models/kimcnn_model_MR')
 
 	model = network.model
 
